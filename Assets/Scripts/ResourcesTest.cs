2339b75172742da366116f45c0e5426e7b2bd845
LSC <314623971@qq.com>
2020-06-27 15:39:22 +0800

    测试资源加载

---
+++ Assets/Scripts/ResourcesTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourcesTest : MonoBehaviour
+{
+
+    public GameObject AttackObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(AttackObj);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
